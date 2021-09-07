cb8fb5923a63c03007a9b3a1febc08b8b7557dd4
Yakumo Saki <yakumo@ziomatrix.org>
2021-09-07 20:51:42 +0900

    fix #14 diskSizeに含まれる単位を削除してbyte単位に

--- modules/parser/cdi_parser_diskdetail.py
+++ modules/parser/cdi_parser_diskdetail.py
@@ -1,5 +1,6 @@
 import logging
-from modules.const import Keys
+
+from modules.const import Unit
 from modules.parser.cdi_const import RS_DISKLIST, RS_DISK_SMART
 
 import copy
@@ -30,7 +31,7 @@ def parse_diskdetail_body(detail, line):
     value = attr[1].strip()
 
     # CDI出力 -> key
-    maps = {
+    keymaps = {
         "Firmware": "firmware",
         "Serial Number": "serialNumber",
         "Disk Size": "diskSize",
@@ -41,75 +42,38 @@ def parse_diskdetail_body(detail, line):
         "Health Status": "healthStatus"
     }
 
-    for k, v in maps.items():
+    convmaps = {
+        "Disk Size": _diskSize
+    }
+
+    for k, v in keymaps.items():
         if (key == k):
-            detail[v] = value
+            if (key in convmaps):
+                # 変換用メソッドがある
+                detail[v] = convmaps[key](value)
+            else:
+                detail[v] = value
+            
+            break
 
     return None
 
 
-def isNvme(detail):
-    return "NVM Express" in detail["interface"]
-
-
-"""ディスク詳細のSMART部を1行解釈してdetail.smartに追加する
-cf) "01 200 200 _51 000000000000 リードエラーレート"
 """
-def parse_diskdetail_smart(detail, line):
-    import copy
-    smart = copy.deepcopy(RS_DISK_SMART)
-
-    # NVMe or else
-    if isNvme(detail):
-        parse_diskdetail_smart_nvme(smart, line)
-    else:
-        parse_diskdetail_smart_non_nvme(smart, line)
-
-    detail["smart"].append(smart)
-
-    return None
-
-"""SMARTの値の桁揃えを解除
-cf) _15 -> 15 __0 -> 0
+パターン1 "250.0 GB (8.4/137.4/250.0/250.0)", SATA
+パターン2 "512.1 GB", NVMe
 """
-def parse_smart_value(numString):
-    return numString.replace("_", "").strip()
+def _diskSize(value):
+    v = value.split(" ", maxsplit=2)
 
+    num = float(v[0])
+    unit = v[1].strip()
 
-"""ディスク詳細のSMART部を1行解釈してdetail.smartに追加する
-cf) "01 200 200 _51 000000000000 リードエラーレート"
-"""
-def parse_diskdetail_smart_non_nvme(smart, line):
+    if (unit == "GB"):
+        return int(num * Unit.GiB)
+    elif (unit == "MB"):
+        return int(num * Unit.MiB)
+    elif (unit == "TB"):
+        return int(num * Unit.TiB)
 
-    ln = line.strip()
-
-    smartAttr = ln.split(" ", maxsplit=5)  # Attribute Name部はスペースが入りうるので個数指定
-    smart["id"] = smartAttr[0].strip()
-    smart["name"] = smartAttr[5].strip()
-    smart["value"] = parse_smart_value(smartAttr[1])
-    smart["worst"] = parse_smart_value(smartAttr[2])
-    smart["threshould"] = parse_smart_value(smartAttr[3])
-
-    return smart
-
-
-def parse_diskdetail_smart_nvme(smart, line):
-
-    ln = line.strip()
-
-    smartAttr = ln.split(" ", maxsplit=2)  # Attribute Name部はスペースが入りうるので個数指定
-    
-    # TODO 調整
-    smart["id"] = smartAttr[0].strip()
-    smart["name"] = smartAttr[2].strip()
-
-    # TODO RawValues -> value
-    smart["value"] = parse_smart_rawvalue(smartAttr[1])
-
-    return smart
-
-
-"""RawValueが16進数なので10進数にする
-"""
-def parse_smart_rawvalue(rawValue):
-    return int(rawValue, 16)
+    return num
--- modules/parser/cdi_parser_smart.py
+++ modules/parser/cdi_parser_smart.py
@@ -7,10 +7,6 @@ import copy
 logger = logging.getLogger(__name__)
 
 
-def isNvme(detail):
-    return "NVM Express" in detail["interface"]
-
-
 """ディスク詳細のSMART部を1行解釈してdetail.smartに追加する
 cf) "01 200 200 _51 000000000000 リードエラーレート"
 """
@@ -19,40 +15,45 @@ def parse_diskdetail_smart(detail, line):
     smart = copy.deepcopy(RS_DISK_SMART)
 
     # NVMe or else
-    if isNvme(detail):
-        parse_diskdetail_smart_nvme(smart, line)
+    if _isNvme(detail):
+        _parse_smart_nvme(smart, line)
     else:
-        parse_diskdetail_smart_non_nvme(smart, line)
+        _parse_smart_non_nvme(smart, line)
 
     detail["smart"].append(smart)
 
     return None
 
+
+def _isNvme(detail):
+    return "NVM Express" in detail["interface"]
+
+
 """SMARTの値の桁揃えを解除
 cf) _15 -> 15 __0 -> 0
 """
-def parse_smart_value(numString):
+def _parse_smart_value(numString):
     return numString.replace("_", "").strip()
 
 
 """ディスク詳細のSMART部を1行解釈してdetail.smartに追加する
 cf) "01 200 200 _51 000000000000 リードエラーレート"
 """
-def parse_diskdetail_smart_non_nvme(smart, line):
+def _parse_smart_non_nvme(smart, line):
 
     ln = line.strip()
 
     smartAttr = ln.split(" ", maxsplit=5)  # Attribute Name部はスペースが入りうるので個数指定
     smart["id"] = smartAttr[0].strip()
     smart["name"] = smartAttr[5].strip()
-    smart["value"] = parse_smart_value(smartAttr[1])
-    smart["worst"] = parse_smart_value(smartAttr[2])
-    smart["threshould"] = parse_smart_value(smartAttr[3])
+    smart["value"] = _parse_smart_value(smartAttr[1])
+    smart["worst"] = _parse_smart_value(smartAttr[2])
+    smart["threshould"] = _parse_smart_value(smartAttr[3])
 
     return smart
 
 
-def parse_diskdetail_smart_nvme(smart, line):
+def _parse_smart_nvme(smart, line):
 
     ln = line.strip()
 
@@ -63,12 +64,12 @@ def parse_diskdetail_smart_nvme(smart, line):
     smart["name"] = smartAttr[2].strip()
 
     # TODO RawValues -> value
-    smart["value"] = parse_smart_rawvalue(smartAttr[1])
+    smart["value"] = _parse_smart_rawvalue(smartAttr[1])
 
     return smart
 
 
 """RawValueが16進数なので10進数にする
 """
-def parse_smart_rawvalue(rawValue):
+def _parse_smart_rawvalue(rawValue):
     return int(rawValue, 16)
