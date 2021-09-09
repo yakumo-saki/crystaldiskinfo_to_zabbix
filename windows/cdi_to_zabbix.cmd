05f2c79888768ba05f973da4f7bf107440947e66
Yakumo Saki <yakumo@ziomatrix.org>
2021-09-09 12:34:39 +0900

    Add windows bat for task sched

--- windows/cdi_to_zabbix.cmd
+++ windows/cdi_to_zabbix.cmd
@@ -2,9 +2,10 @@
 taskkill /F /IM DISKINFO64.exe
 taskkill /F /IM DISKINFO32.exe
 
+del "C:\Program Files\CrystalDiskInfo\DiskInfo.txt"
 start "no title" "C:\Program Files\CrystalDiskInfo\DiskInfo64.exe" /Copy
 
-rem wait for CDI write DiskInfo.txt
+echo "Wait for CrystalDiskInfo write DiskInfo.txt"
 timeout /T 10 /NOBREAK > nul
 
 python C:\usr\src\crystaldiskinfo_to_zabbix\crystaldiskinfo_to_zabbix.py
